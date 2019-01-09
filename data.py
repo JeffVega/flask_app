1b73f2a6d3030b669682255bfe67e163f3e02d44
JeffVega <jeff.vega321@gmail.com>
2019-01-08 19:22:28 -0800

    dummy data

---
+++ data.py
@@ -0,0 +1,22 @@
+def Articles():
+    articles = [
+        {
+  "id": 1,
+  "title": "Future University",
+  "body": "Synergized 6th generation neural-net",
+  "author": "Lukas Dosdill",
+  "create_date": "01-08-2019"
+}, {
+  "id": 2,
+  "title": "Kushiro Public University of Economics",
+  "body": "Decentralized reciprocal archive",
+  "author": "Caleb Reeken",
+  "create_date": "01-08-2019"
+}, {
+  "id": 3,
+  "title": "Athens Laboratory of Business Administration (ALBA)",
+  "body": "Pre-emptive high-level attitude",
+  "author": "Dena McArthur",
+  "create_date": "01-08-2019"
+}
+    ]
